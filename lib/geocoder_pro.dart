8c6e959b1cca26c26b633b1a1fd20e14b9fa80b2
Rohit Soni <70101613+rohitsoni9@users.noreply.github.com>
2025-04-15 05:03:59 +0000

    Initial Commit

---
+++ lib/geocoder_pro.dart
@@ -0,0 +1,4 @@
+library geocoder_pro;
+
+export 'src/geocoder_pro.dart';
+export 'src/modal/data.dart';
---
+++ test/geocoder_pro_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:geocoder_pro/geocoder_pro.dart';
+
+void main() {
+  test('adds one to input values', () {
+    final calculator = GeocoderPro();
+  });
+}
